437253d221729d09b6117e00e69fbc15f8817d56
Rob Carney <robert.edward.carney@gmail.com>
2018-11-24 14:10:46 -0500

    Added simple healthcheck

--- server.go
+++ server.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func healthcheck(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Healthcheck succeeded")
+}
+
 func main() {
 	directory := "/Users/robertcarney/tmp/server-test";
-	err := http.ListenAndServe(":8080", http.FileServer(http.Dir(directory)))
+	http.HandleFunc("/healthcheck", healthcheck)
+	http.Handle("/", http.FileServer(http.Dir(directory)))
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Listen and serve: ", err)
 	}
