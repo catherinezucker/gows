4a93f999f497a1f3d40ffc9a3a35ba2c48669800
Rob Carney <robert.edward.carney@gmail.com>
2018-11-29 22:32:36 -0500

    First pass at making a channel of servers

--- master.go
+++ master.go
@@ -2,26 +2,84 @@ package main
 
 import(
 	"fmt"
-	"log"
+	"os"
 	"os/exec"
+	"os/signal"
+	"strconv"
+	"time"
 )
 
-func startServer(dir, port string)  {
-	for true  {
-		cmd := exec.Command("./server", dir, port)
-		err := cmd.Start()
-		if err != nil {
-			log.Fatal(err)
+// ServerJob represents a server worker process
+type ServerJob struct {
+	port int
+	dir string
+	command *exec.Cmd
+}
+
+func startServer(dir string, port int, serverJobs chan ServerJob)  {
+	cmd := exec.Command("./server", dir, strconv.Itoa(port))
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+	serverJobs <- ServerJob{port, dir, cmd}
+	fmt.Printf("Started server for directory %s at port :%d\n", dir, port)
+	err = cmd.Wait()
+	fmt.Printf("Server at port :%d exited with error code %v\n", 
+		port, err)
+}
+
+func initServers(serverJobs chan ServerJob)  {
+	go startServer("/Users/robertcarney", 9090, serverJobs)
+	go startServer("/Users/robertcarney", 9091, serverJobs)
+}
+
+func monitorServers(serverJobs chan ServerJob, quitChannel chan bool)  {
+	for  {
+		select {
+		case <-quitChannel:
+			fmt.Println("Recieved quit signal")
+			close(quitChannel)
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("Looking for a server to monitor")
+			currentJob := <- serverJobs
+			fmt.Printf("Looking at server on port: %d\n", currentJob.port)
+			if (currentJob.command == nil) {
+				fmt.Println("Server process exited")
+				continue
+			}
+			serverJobs <- currentJob
+			fmt.Printf("Done monitoring server at port %d\n", currentJob.port)
 		}
-		fmt.Printf("Started server for directory %s at port :%s\n", dir, port)
-		err = cmd.Wait()
-		fmt.Println("Server at port :%s exited with error code %v, attempting to start again", 
-			port, err)
 	}
 }
 
+func cleanUpOnSignal(signals chan os.Signal, serverJobs chan ServerJob, quitChannels []chan bool)  {
+	defer close(signals)
+	<-signals
+	for _, quitChannel := range quitChannels {
+		quitChannel <- true
+	}
+	close(serverJobs)
+	for currentJob := range serverJobs {
+		if (currentJob.command != nil) {
+			currentJob.command.Process.Kill()
+		}
+	}
+	os.Exit(1)
+}
+
 func main()  {
-	go startServer("/Users/robertcarney/tmp", "9000")
-	go startServer("/Users/robertcarney/tmp", "9001")
-	for true {  }
+	serverJobs := make(chan ServerJob, 2)
+	var quitChannels []chan bool
+	go initServers(serverJobs)
+	monitorServersQuitChannel := make(chan bool)
+	quitChannels = append(quitChannels, monitorServersQuitChannel)
+	go monitorServers(serverJobs, monitorServersQuitChannel)
+	signals := make(chan os.Signal, 2)
+	signal.Notify(signals, os.Interrupt, os.Kill)
+	go cleanUpOnSignal(signals, serverJobs, quitChannels)
+	for {  }
 }
